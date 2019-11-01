d2a96795506bdabb6e757db5b8b82fa4e7618161
Nick Gheorghita <nickgheorghita@gmail.com>
2019-11-01 09:14:06 -0400

    Update pkg variables to package

--- ethpm_cli/commands/install.py
+++ ethpm_cli/commands/install.py
@@ -31,24 +31,24 @@ from ethpm_cli.validation import validate_parent_directory, validate_same_regist
 logger = logging.getLogger("ethpm_cli.install")
 
 
-def install_package(pkg: Package, config: Config) -> None:
-    if is_package_installed(pkg.alias, config):
+def install_package(package: Package, config: Config) -> None:
+    if is_package_installed(package.alias, config):
         raise InstallError(
-            f"Installation conflict: Package: '{pkg.manifest['package_name']}' "
-            f"aliased to '{pkg.alias}' already installed on the filesystem at "
-            f"{config.ethpm_dir / pkg.alias}. Try installing this package with "
+            f"Installation conflict: Package: '{package.manifest['package_name']}' "
+            f"aliased to '{package.alias}' already installed on the filesystem at "
+            f"{config.ethpm_dir / package.alias}. Try installing this package with "
             "a different alias."
         )
 
     # Create temporary package directory
-    tmp_pkg_dir = Path(tempfile.mkdtemp())
-    write_pkg_installation_files(pkg, tmp_pkg_dir, config.ipfs_backend)
+    tmp_package_dir = Path(tempfile.mkdtemp())
+    write_package_installation_files(package, tmp_package_dir, config.ipfs_backend)
 
     # Copy temp package directory to ethpm dir namespace
-    dest_pkg_dir = config.ethpm_dir / pkg.alias
-    validate_parent_directory(config.ethpm_dir, dest_pkg_dir)
-    shutil.copytree(tmp_pkg_dir, dest_pkg_dir)
-    install_to_ethpm_lock(pkg, (config.ethpm_dir / LOCKFILE_NAME))
+    dest_package_dir = config.ethpm_dir / package.alias
+    validate_parent_directory(config.ethpm_dir, dest_package_dir)
+    shutil.copytree(tmp_package_dir, dest_package_dir)
+    install_to_ethpm_lock(package, (config.ethpm_dir / LOCKFILE_NAME))
 
 
 class InstalledPackageTree(NamedTuple):
@@ -90,8 +90,8 @@ def list_installed_packages(config: Config) -> None:
         for base_dir in config.ethpm_dir.iterdir()
         if base_dir.is_dir()
     ]
-    for pkg in sorted(installed_packages):
-        logger.info(pkg.format_for_display)
+    for package in sorted(installed_packages):
+        logger.info(package.format_for_display)
 
 
 def get_installed_package_tree(base_dir: Path, depth: int = 0) -> InstalledPackageTree:
@@ -121,93 +121,110 @@ def get_package_aliases(package_name: str, config: Config) -> Iterable[Tuple[str
     if lockfile_path.is_file():
         lockfile = json.loads(lockfile_path.read_text())
         all_aliases = [
-            (pkg_data["alias"], pkg_data["resolved_package_name"])
-            for pkg_data in lockfile.values()
+            (package_data["alias"], package_data["resolved_package_name"])
+            for package_data in lockfile.values()
         ]
-        for alias, resolved_pkg_name in all_aliases:
-            if resolved_pkg_name == package_name:
+        for alias, resolved_package_name in all_aliases:
+            if resolved_package_name == package_name:
                 yield alias
 
 
-def resolve_pkg_name_or_alias(pkg_name: str, config: Config) -> InstalledPackage:
+def resolve_installed_package_by_id(
+    package_id: str, config: Config
+) -> InstalledPackage:
     lockfile_path = config.ethpm_dir / "ethpm.lock"
     lockfile = json.loads(lockfile_path.read_text())
-    return InstalledPackage(**lockfile[pkg_name])
+    return InstalledPackage(**lockfile[package_id])
 
 
 def update_package(args: Namespace, config: Config) -> None:
-    if is_package_installed(args.package, config):
-        installed_pkg = resolve_pkg_name_or_alias(args.package, config)
-        active_registry = get_active_registry(config.xdg_ethpmcli_root / REGISTRY_STORE)
-        if is_valid_registry_uri(installed_pkg.install_uri):
-            validate_same_registry(installed_pkg.install_uri, active_registry.uri)
-
-        connected_chain_id = config.w3.eth.chainId
-        active_registry_uri = parse_registry_uri(active_registry.uri)
-        if not to_int(text=active_registry_uri.chain_id) == connected_chain_id:
-            raise InstallError(
-                f"Registry URI chain: {active_registry_uri.chain_id} doesn't match "
-                f"connected web3: {connected_chain_id}."
-            )
-
-        config.w3.pm.set_registry(active_registry_uri.address)
-        all_pkg_names = config.w3.pm.get_all_package_names()
-        if installed_pkg.resolved_package_name not in all_pkg_names:
-            raise InstallError(
-                f"{installed_pkg.resolved_package_name} is not available on the active registry "
-                f"{active_registry.uri}. Available packages include: {all_pkg_names}."
-            )
-
-        all_release_data = config.w3.pm.get_all_package_releases(
-            installed_pkg.resolved_package_name
-        )
-        all_versions = [version for version, _ in all_release_data]
-
-        if installed_pkg.resolved_version not in all_versions:
-            raise InstallError(
-                f"{installed_pkg.resolved_package_name}@{installed_pkg.resolved_version} not "
-                f"found on the active registry {active_registry.uri}."
-            )
-
-        on_chain_install_uri = pluck_release_data(
-            all_release_data, installed_pkg.resolved_version
-        )
-        if on_chain_install_uri != installed_pkg.resolved_uri:
-            raise InstallError(
-                f"Install URI found on active registry for {installed_pkg.resolved_package_name}@"
-                f"{installed_pkg.resolved_version}: {on_chain_install_uri} does not match the "
-                f"install URI found in local lockfile: {installed_pkg.resolved_uri}."
-            )
-
-        cli_logger.info(
-            f"{len(all_versions)} versions of {installed_pkg.resolved_package_name} "
-            f"found on the active registry: {all_versions}."
-        )
-        while True:
-            target_version = input("Please enter the version you want to install. ")
-            if target_version == installed_pkg.resolved_version:
-                cli_logger.info(f"Version already installed: {target_version}. ")
-            elif target_version not in all_versions:
-                cli_logger.info(f"Version unavailable: {target_version}. ")
-            else:
-                break
-
-        # Create an updated args/Package for new install
-        updated_args = copy.deepcopy(args)
-        if installed_pkg.resolved_package_name != args.package:
-            updated_args.alias = args.package
-        updated_args.uri = pluck_release_data(all_release_data, target_version)
-        updated_args.package_version = target_version
-        updated_package = Package(updated_args, config.ipfs_backend)
-
-        uninstall_package(args.package, config)
-        install_package(updated_package, config)
-        cli_logger.info(
-            f"{updated_args.package} successfully updated to version "
-            f"{updated_args.package_version}."
-        )
-    else:
+    if not is_package_installed(args.package, config):
         check_for_aliased_package(args.package, config)
+        return
+
+    installed_package = resolve_installed_package_by_id(args.package, config)
+    active_registry = get_active_registry(config.xdg_ethpmcli_root / REGISTRY_STORE)
+    if is_valid_registry_uri(installed_package.install_uri):
+        validate_same_registry(installed_package.install_uri, active_registry.uri)
+
+    connected_chain_id = config.w3.eth.chainId
+    active_registry_uri = parse_registry_uri(active_registry.uri)
+    if not to_int(text=active_registry_uri.chain_id) == connected_chain_id:
+        raise InstallError(
+            f"Registry URI chain: {active_registry_uri.chain_id} doesn't match "
+            f"connected web3: {connected_chain_id}."
+        )
+
+    config.w3.pm.set_registry(active_registry_uri.address)
+    all_package_names = config.w3.pm.get_all_package_names()
+    if installed_package.resolved_package_name not in all_package_names:
+        raise InstallError(
+            f"{installed_package.resolved_package_name} is not available on the active registry "
+            f"{active_registry.uri}. Available packages include: {all_package_names}."
+        )
+
+    all_release_data = config.w3.pm.get_all_package_releases(
+        installed_package.resolved_package_name
+    )
+    all_versions = [version for version, _ in all_release_data]
+
+    if installed_package.resolved_version not in all_versions:
+        raise InstallError(
+            f"{installed_package.resolved_package_name}@{installed_package.resolved_version} not "
+            f"found on the active registry {active_registry.uri}."
+        )
+
+    on_chain_install_uri = pluck_release_data(
+        all_release_data, installed_package.resolved_version
+    )
+    if on_chain_install_uri != installed_package.resolved_uri:
+        raise InstallError(
+            f"Install URI found on active registry for {installed_package.resolved_package_name}@"
+            f"{installed_package.resolved_version}: {on_chain_install_uri} does not match the "
+            f"install URI found in local lockfile: {installed_package.resolved_uri}."
+        )
+
+    cli_logger.info(
+        f"{len(all_versions)} versions of {installed_package.resolved_package_name} "
+        f"found: {all_versions} \n"
+        f"On the active registry: {active_registry.uri}"
+    )
+    count = 0
+    while True:
+        count += 1
+        target_version = input("Please enter the version you want to install. ")
+        if count > 5:
+            raise InstallError("Max attempts (5) reached. ")
+        elif target_version == installed_package.resolved_version:
+            cli_logger.info(f"Version already installed: {target_version}. ")
+        elif target_version not in all_versions:
+            cli_logger.info(f"Version unavailable: {target_version}. ")
+        else:
+            break
+
+    # Create an updated args/Package for new install
+    updated_args = copy.deepcopy(args)
+    if installed_package.resolved_package_name != args.package:
+        updated_args.alias = args.package
+    updated_args.uri = pluck_release_data(all_release_data, target_version)
+    updated_args.package_version = target_version
+    updated_package = Package(updated_args, config.ipfs_backend)
+
+    # atomic replace
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_ethpm_dir = Path(tmpdir) / ETHPM_PACKAGES_DIR
+        shutil.copytree(config.ethpm_dir, tmp_ethpm_dir)
+        tmp_config = copy.copy(config)
+        tmp_config.ethpm_dir = tmp_ethpm_dir
+        uninstall_package(args.package, tmp_config)
+        install_package(updated_package, tmp_config)
+        shutil.rmtree(config.ethpm_dir)
+        tmp_ethpm_dir.replace(config.ethpm_dir)
+
+    cli_logger.info(
+        f"{updated_args.package} successfully updated to version "
+        f"{updated_args.package_version}."
+    )
 
 
 def pluck_release_data(all_release_data: List[str], target_version: str) -> URI:
@@ -217,17 +234,17 @@ def pluck_release_data(all_release_data: List[str], target_version: str) -> URI:
 
 
 def uninstall_package(package_name: str, config: Config) -> None:
-    if is_package_installed(package_name, config):
-        tmp_pkg_dir = Path(tempfile.mkdtemp()) / ETHPM_PACKAGES_DIR
-        shutil.copytree(config.ethpm_dir, tmp_pkg_dir)
-        shutil.rmtree(tmp_pkg_dir / package_name)
-        uninstall_from_ethpm_lock(package_name, (tmp_pkg_dir / LOCKFILE_NAME))
-
-        shutil.rmtree(config.ethpm_dir)
-        tmp_pkg_dir.replace(config.ethpm_dir)
-        return
-    else:
+    if not is_package_installed(package_name, config):
         check_for_aliased_package(package_name, config)
+        return
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_package_dir = Path(tmpdir) / ETHPM_PACKAGES_DIR
+        shutil.copytree(config.ethpm_dir, tmp_package_dir)
+        shutil.rmtree(tmp_package_dir / package_name)
+        uninstall_from_ethpm_lock(package_name, (tmp_package_dir / LOCKFILE_NAME))
+        shutil.rmtree(config.ethpm_dir)
+        tmp_package_dir.replace(config.ethpm_dir)
 
 
 def check_for_aliased_package(package_name: str, config: Config) -> None:
@@ -243,38 +260,38 @@ def check_for_aliased_package(package_name: str, config: Config) -> None:
         )
 
 
-def write_pkg_installation_files(
-    pkg: Package, tmp_pkg_dir: Path, ipfs_backend: BaseIPFSBackend
+def write_package_installation_files(
+    package: Package, tmp_package_dir: Path, ipfs_backend: BaseIPFSBackend
 ) -> None:
-    (tmp_pkg_dir / "manifest.json").touch()
-    (tmp_pkg_dir / "manifest.json").write_bytes(pkg.raw_manifest)
+    (tmp_package_dir / "manifest.json").touch()
+    (tmp_package_dir / "manifest.json").write_bytes(package.raw_manifest)
 
-    write_sources_to_disk(pkg, tmp_pkg_dir, ipfs_backend)
-    write_docs_to_disk(pkg, tmp_pkg_dir, ipfs_backend)
-    write_build_deps_to_disk(pkg, tmp_pkg_dir, ipfs_backend)
-    tmp_ethpm_lock = tmp_pkg_dir.parent / LOCKFILE_NAME
-    install_to_ethpm_lock(pkg, tmp_ethpm_lock)
+    write_sources_to_disk(package, tmp_package_dir, ipfs_backend)
+    write_docs_to_disk(package, tmp_package_dir, ipfs_backend)
+    write_build_deps_to_disk(package, tmp_package_dir, ipfs_backend)
+    tmp_ethpm_lock = tmp_package_dir.parent / LOCKFILE_NAME
+    install_to_ethpm_lock(package, tmp_ethpm_lock)
 
 
 def write_sources_to_disk(
-    pkg: Package, pkg_dir: Path, ipfs_backend: BaseIPFSBackend
+    package: Package, package_dir: Path, ipfs_backend: BaseIPFSBackend
 ) -> None:
-    sources = resolve_sources(pkg, ipfs_backend)
+    sources = resolve_sources(package, ipfs_backend)
     for path, source_contents in sources.items():
-        target_file = pkg_dir / SRC_DIR_NAME / path
+        target_file = package_dir / SRC_DIR_NAME / path
         target_dir = target_file.parent
         if not target_dir.is_dir():
             target_dir.mkdir(parents=True)
         target_file.touch()
-        validate_parent_directory((pkg_dir / SRC_DIR_NAME), target_file)
+        validate_parent_directory((package_dir / SRC_DIR_NAME), target_file)
         target_file.write_text(source_contents)
 
 
 @to_dict
 def resolve_sources(
-    pkg: Package, ipfs_backend: BaseIPFSBackend
+    package: Package, ipfs_backend: BaseIPFSBackend
 ) -> Iterable[Tuple[str, str]]:
-    for path, source in pkg.manifest["sources"].items():
+    for path, source in package.manifest["sources"].items():
         if is_ipfs_uri(source):
             contents = to_text(ipfs_backend.fetch_uri_contents(source)).rstrip("\n")
         else:
@@ -284,42 +301,42 @@ def resolve_sources(
 
 
 def write_docs_to_disk(
-    pkg: Package, pkg_dir: Path, ipfs_backend: BaseIPFSBackend
+    package: Package, package_dir: Path, ipfs_backend: BaseIPFSBackend
 ) -> None:
     try:
-        doc_uri = pkg.manifest["meta"]["links"]["documentation"]
+        doc_uri = package.manifest["meta"]["links"]["documentation"]
     except KeyError:
         return
 
     if is_ipfs_uri(doc_uri):
         documentation = ipfs_backend.fetch_uri_contents(doc_uri)
-        doc_path = pkg_dir / "documentation.md"
+        doc_path = package_dir / "documentation.md"
         doc_path.touch()
         doc_path.write_bytes(documentation)
 
 
 def write_build_deps_to_disk(
-    pkg: Package, pkg_dir: Path, ipfs_backend: BaseIPFSBackend
+    package: Package, package_dir: Path, ipfs_backend: BaseIPFSBackend
 ) -> None:
-    if "build_dependencies" in pkg.manifest:
-        child_ethpm_dir = pkg_dir / ETHPM_PACKAGES_DIR
+    if "build_dependencies" in package.manifest:
+        child_ethpm_dir = package_dir / ETHPM_PACKAGES_DIR
         child_ethpm_dir.mkdir()
-        for name, uri in pkg.manifest["build_dependencies"].items():
-            dep_pkg = Package(Namespace(uri=uri, alias=""), ipfs_backend)
+        for name, uri in package.manifest["build_dependencies"].items():
+            dep_package = Package(Namespace(uri=uri, alias=""), ipfs_backend)
             tmp_dep_dir = child_ethpm_dir / name
             tmp_dep_dir.mkdir()
-            validate_parent_directory(pkg_dir, tmp_dep_dir)
-            write_pkg_installation_files(dep_pkg, tmp_dep_dir, ipfs_backend)
+            validate_parent_directory(package_dir, tmp_dep_dir)
+            write_package_installation_files(dep_package, tmp_dep_dir, ipfs_backend)
 
 
-def install_to_ethpm_lock(pkg: Package, ethpm_lock: Path) -> None:
+def install_to_ethpm_lock(package: Package, ethpm_lock: Path) -> None:
     if ethpm_lock.is_file():
         old_lock = json.loads(ethpm_lock.read_text())
     else:
         old_lock = {}
         ethpm_lock.touch()
-    new_pkg_data = pkg.generate_ethpm_lock()
-    new_lock = assoc(old_lock, pkg.alias, new_pkg_data)
+    new_package_data = package.generate_ethpm_lock()
+    new_lock = assoc(old_lock, package.alias, new_package_data)
     ethpm_lock.write_text(f"{json.dumps(new_lock, sort_keys=True, indent=4)}\n")
 
 
