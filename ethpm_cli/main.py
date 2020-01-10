f90471d3b3342e462797d35b8775c3d14803bfa9
Nick Gheorghita <nickgheorghita@gmail.com>
2020-01-10 22:54:01 +0100

    Add ethpm logo

--- ethpm_cli/main.py
+++ ethpm_cli/main.py
@@ -3,8 +3,19 @@ from ethpm_cli.constants import ETHPM_CLI_VERSION
 from ethpm_cli.parser import parser
 
 
+COLOR_RESET = '\x1b[0m'
+ETHPM_LOGO = """
+       _   _     ____  __  __ 
+   ___| |_| |__ |  _ \|  \/  |
+  / _ \ __| '_ \| |_) | |\/| |
+ |  __/ |_| | | |  __/| |  | |
+  \___|\__|_| |_|_|   |_|  |_|
+                              
+"""
+
 def main() -> None:
-    cli_logger.info(f"ethPM CLI v{ETHPM_CLI_VERSION}\n")
+    cli_logger.info('\033[01;32m' + ETHPM_LOGO + COLOR_RESET)
+    cli_logger.info(f"A command line tool for the Ethereum Package Manager. v{ETHPM_CLI_VERSION}\n")
 
     args = parser.parse_args()
 
